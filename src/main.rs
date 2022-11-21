00b80b50349dc60ea9ae0b7902e820954f479539
Chase Guynup <chasepg0821@gmail.com>
2022-11-21 15:07:36 -0500

    async and gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use clap::Parser;
-use std::path::PathBuf;
+use colored::Colorize;
+use spinners::{Spinner, Spinners};
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
-use spinners::{Spinner, Spinners};
+use std::path::PathBuf;
 use std::process;
-use colored::Colorize;
 
 // ======================== TODO IF WE CAN GET TO IT ========================================
 // *Allow filtering of output by response codes via passed args - Mark Elkins can complete this too
 // *Colorize output i.e green status codes for 200 ok, red for 404 not found - Mark Elkins will complete this (200 and 404 done)
-// Actually make it use async for speed
+// Actually make it use async for speed - Chase is currently doing this
 // *Cool logo to display via ASCII art on startup
 // Filter output while requests are running <- fancy hard async stuff I doubt we do but would be awesome
 // Recurse argument to bust directories found by the current bust
@@ -19,13 +19,13 @@ use colored::Colorize;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   /// url of site to bust
-   #[arg(short, long)]
-   url: String,
+    /// url of site to bust
+    #[arg(short, long)]
+    url: String,
 
-   /// path to wordlist of directories and files to try
-   #[arg(short, long)]
-   wordlist: PathBuf,
+    /// path to wordlist of directories and files to try
+    #[arg(short, long)]
+    wordlist: PathBuf,
 }
 
 fn output_art() {
@@ -35,29 +35,25 @@ fn output_art() {
     art_file.read_to_string(&mut art).unwrap();
 
     println!("{}\n", art.truecolor(245, 102, 0));
-
 }
 
 /// Makes a request to the given full_path
 /// Outputs the status code of the request or
 /// Outputs Error if request failed
-async fn make_request(full_path: &String) {    
-    let _res = match reqwest::get(full_path).await{
-        Ok(res)=>{
-            if res.status() ==  200 {
+async fn make_request(full_path: &String) {
+    let _res = match reqwest::get(full_path).await {
+        Ok(res) => {
+            if res.status() == 200 {
                 println!("\r[{}] - {}", res.status().as_str().green(), full_path);
-            }
-            else if res.status() == 404 || res.status() == 406 {
+            } else if res.status() == 404 || res.status() == 406 {
                 println!("\r[{}] - {}", res.status().as_str().red(), full_path);
-            }
-            else if res.status() == 403 {
+            } else if res.status() == 403 {
                 println!("\r[{}] - {}", res.status().as_str().yellow(), full_path);
-            }   
-            else{
+            } else {
                 println!("\r[{}] - {}", res.status(), full_path);
             }
         }
-        Err(_e)=>{
+        Err(_e) => {
             println!("\r{}", "[Error] Make sure given url exists".red());
             process::exit(0);
         }
@@ -70,15 +66,25 @@ async fn main() {
     let args = Args::parse();
 
     // Displays loading animation
-    let mut sp = Spinner::new(Spinners::Triangle,"Searching...".into());
+    let mut sp = Spinner::new(Spinners::Triangle, "Searching...".into());
+
+    //create a vector of concurrent task handlers
+    let mut task_handlers = vec![];
 
     let wordlist = File::open(args.wordlist).unwrap();
     let reader = BufReader::new(wordlist);
-    for (_index, line) in reader.lines().enumerate(){
+    for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         let full_path = format!("{}/{}", args.url, line);
 
-        make_request(&full_path).await;
+        let handler = tokio::spawn(async move {
+            make_request(&full_path).await;
+        });
+        task_handlers.push(handler);
+    }
+
+    for handler in task_handlers {
+        handler.await.unwrap();
     }
 
     sp.stop_and_persist("✅", " Finished search!".green().to_string());
