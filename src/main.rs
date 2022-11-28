c6fb4aa09d689798ca3f025b4d05f760f86140bd
Chase Guynup <chasepg0821@gmail.com>
2022-11-28 17:14:57 -0500

    steps towards recurrions

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,22 @@ async fn bust_url(
     base_path: &String,
     word_list: &Vec<String>,
     file_extensions: &Vec<String>,
-) -> (u16, String) {
+    include_list: &Vec<u16>,
+    exclude_list: &Vec<u16>,
+) {
+    let mut task_handlers = Vec::new();
+
+    for word in word_list {
+        let handler = tokio::spawn(async move {
+            let request_path = format!("{}/{}", base_path, word);
+            let (status, path) = make_request(&request_path).await;
+        });
+        task_handlers.push(handler);
+    }
+
+    for handler in task_handlers {
+        handler.await.unwrap();
+    }
 }
 
 #[tokio::main]
@@ -107,65 +122,11 @@ async fn main() {
     let word_file = File::open(args.wordlist).unwrap();
     let reader = BufReader::new(word_file);
     for (_index, line) in reader.lines().enumerate() {
-        word_list.append(line);
+        word_list.push(line.unwrap());
     }
 
     let file_extensions: Vec<String> = args.extensions.clone().unwrap_or_else(|| {
         vec![
-            String::from("AIFF"),
-            String::from("AIF"),
-            String::from("AU"),
-            String::from("AVI"),
-            String::from("BAT"),
-            String::from("BMP"),
-            String::from("CLASS"),
-            String::from("JAVA"),
-            String::from("CSV"),
-            String::from("CVS"),
-            String::from("DBF"),
-            String::from("DIF"),
-            String::from("DOC"),
-            String::from("DOCX"),
-            String::from("EPS"),
-            String::from("EXE"),
-            String::from("FM3"),
-            String::from("GIF"),
-            String::from("HQX"),
-            String::from("HTM"),
-            String::from("HTML"),
-            String::from("JPG"),
-            String::from("JPEG"),
-            String::from("MAC"),
-            String::from("MAP"),
-            String::from("MDB"),
-            String::from("MID"),
-            String::from("MIDI"),
-            String::from("MOV"),
-            String::from("QT"),
-            String::from("MTB"),
-            String::from("MTW"),
-            String::from("PDF"),
-            String::from("P65"),
-            String::from("T65"),
-            String::from("PNG"),
-            String::from("PPT"),
-            String::from("PPTX"),
-            String::from("PSD"),
-            String::from("PSP"),
-            String::from("QXD"),
-            String::from("RA"),
-            String::from("RTF"),
-            String::from("SIT"),
-            String::from("TAR"),
-            String::from("TIF"),
-            String::from("TXT"),
-            String::from("WAV"),
-            String::from("WK3"),
-            String::from("WKS"),
-            String::from("WPD"),
-            String::from("WP5"),
-            String::from("XLSX"),
-            String::from("XLSX"),
             String::from("aiff"),
             String::from("aif"),
             String::from("au"),
@@ -223,23 +184,18 @@ async fn main() {
         ]
     });
 
-    //create a vector of concurrent task handlers
-    let mut task_handlers = vec![];
+    let base_path = args.url;
+    let include_list = args.include.clone().unwrap_or_else(|| Vec::new());
+    let exclude_list = args.exclude.clone().unwrap_or_else(|| Vec::new());
 
-    for word in word_list {
-        let base_path = args.url;
-        let include_list = args.include.clone().unwrap_or_else(|| Vec::new());
-        let exclude_list = args.exclude.clone().unwrap_or_else(|| Vec::new());
-
-        let handler = tokio::spawn(async move {
-            bust_url(&base_path, &include_list, &exclude_list).await;
-        });
-        task_handlers.push(handler);
-    }
-
-    for handler in task_handlers {
-        handler.await.unwrap();
-    }
+    bust_url(
+        &base_path,
+        &word_list,
+        &file_extensions,
+        &include_list,
+        &exclude_list,
+    )
+    .await;
 
     sp.stop_and_persist("✅", " Finished search!".green().to_string());
 }
